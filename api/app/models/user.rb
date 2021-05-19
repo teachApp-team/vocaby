0cb0aa70df7f5fa5648b2aef653289d3bce3f633
halhal <hh.25799623@gmail.com>
2021-05-19 11:11:47 +0900

    install and exec annotate

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string(255)
+#  name            :string(255)
+#  password_digest :string(255)
+#  school_year     :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  validates :name, presence :true
+  validates :email, presence :true
+  validates :password_digest, presence :true
 end

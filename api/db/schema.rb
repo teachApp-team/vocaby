2656823d2a44409723753b6197b4c034e25bf762
halhal <hh.25799623@gmail.com>
2021-05-19 11:04:08 +0900

    add User model

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_19_015828) do
+
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "school_year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
